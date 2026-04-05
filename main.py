b771dbb7116ebc7bde246f8ccb667e733c4bc2a8
Kenneth Kiarie <mwangikennethkiarie@gmail.com>
2026-04-05 23:31:01 +0300

    blog

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/blog")
 
-def read_root():
-    return {"Hello": "World"}
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    
+    if published:
+
+      return {'data': f'{limit} published blogs from the DB'}
+    else:
+        return {'data': f'{limit} blogs from the DB'}
 
 
-@app.get("/about")
-def about():
-    return {'data':{'about page'}}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return{'data': 'all unpublished blogs'}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+
+    # fetch blog with id = id
+    return {'data': id}
+
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments of blogs with id = id
+    
+
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = True
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+
+    return{'data': f'Blog is created with title as {blog.title}'}
+    

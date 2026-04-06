886050b7533b29630bdf981eae9b149e630f3ab3
Kenneth Kiarie <mwangikennethkiarie@gmail.com>
2026-04-06 22:54:47 +0300

    API router path operators

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -3,13 +3,15 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from .. import schemas, database, models
 from sqlalchemy.orm import Session
 from ..hashing import Hash
-router = APIRouter()
+router = APIRouter(
+    prefix="/user",
+    tags=['Users'])
 get_db = database.get_db
 
 
 
 
-@router.post('/user', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser, tags=["users"])
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
 
     new_user = models.User(
@@ -20,7 +22,7 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.get('/user/{id}', response_model=schemas.ShowUser, tags=["users"])
+@router.get('/{id}', response_model=schemas.ShowUser)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:

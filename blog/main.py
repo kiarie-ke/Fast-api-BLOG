f745d065490e7ed1eabab82393972c52e9558fd8
Kenneth Kiarie <mwangikennethkiarie@gmail.com>
2026-04-06 14:18:10 +0300

    REsponsemodel

--- blog/main.py
+++ blog/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from typing import List
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
@@ -44,12 +45,12 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200,response_model=schemas.ShowBlog)
 def show(id, response: Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id ==id).first()
     if not blog:

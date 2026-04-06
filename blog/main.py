79e0416b875f349f1db13bfddc4ce9c236f2e9b8
Kenneth Kiarie <mwangikennethkiarie@gmail.com>
2026-04-06 13:55:48 +0300

    updating main.py

--- blog/main.py
+++ blog/main.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -14,12 +16,32 @@ def get_db():
 
 
 @app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db : Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title,body=request.body)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with {id} not found")
+    blog.update(request.model_dump())
+    db.commit()
+    return 'updated'
 
 
 @app.get('/blog')

73b0a8fe1b2534bcd2dd859b19e77737423bb6bf
Kenneth Kiarie <mwangikennethkiarie@gmail.com>
2026-04-07 12:17:17 +0300

    security part

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from .. import schemas, database, models, token
 from ..hashing import Hash
@@ -6,7 +7,7 @@ from ..hashing import Hash
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login', response_model=schemas.Token)
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
